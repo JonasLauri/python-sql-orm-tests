a7d2ee665ce2fdbea9fa57ff3690da0fd156d7d9
jonasllaurinai@gmail.com <jonasllaurinai@gmail.com>
2020-05-21 15:29:34 +0300

    python sql statments

--- pythonMySQL/test.py
+++ pythonMySQL/test.py
@@ -1,12 +1,31 @@
-import mysql.connector
+import mysql.connector as mysql
 import config
 
-
-db = mysql.connector.connect(
+# mysql database connection with mysql.connector
+db = mysql.connect(
     host="localhost",
     user="root",
     passwd=config.USER_PASS,
+    database="testdb", # select your database
 )
 
-print(db)
+# create object of 'cursor' class which help write sql statments with python
+cursor = db.cursor()
+
+# create database
+#cursor.execute("CREATE DATABASE testdb")
+  
+# show list of databases
+'''cursor.execute("SHOW DATABASES")
+databases = cursor.fetchall()
+for database in databases:
+    print(database)'''
+
+# make table to 'testdb'
+'''cursor.execute("CREATE TABLE students (name VARCHAR(255), age INTEGER(10))")'''
+
+# show tables
+'''cursor.execute("SHOW TABLES")'''
+
+
     
